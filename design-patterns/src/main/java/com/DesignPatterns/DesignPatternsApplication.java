6d0e41be81be9019b0cec86fb3cc3966382a65da
chanchalrathore <chanchal.rathor@telekom-digital.com>
2025-06-30 22:10:49 +0530

    design patterns

--- design-patterns/src/main/java/com/DesignPatterns/DesignPatternsApplication.java
+++ design-patterns/src/main/java/com/DesignPatterns/DesignPatternsApplication.java
@@ -1,4 +1,4 @@
-package com.DesignPatterns.design_patterns;
+package com.DesignPatterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
